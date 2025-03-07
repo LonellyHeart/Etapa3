596af49a1d17741cc6d34ef622894475afe26ae8
“Matheus <matheus.ricci@hotmail.com>
2025-03-07 08:37:21 -0300

    Versão final do programa, Etapa 4

---
+++ src/etapa3/dao/FornecedorDao.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etapa3.dao;
+
+import etapa3.beans.Fornecedor;
+import etapa3.conexao.Conexao;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author HEARTHLONELY
+ */
+public class FornecedorDao {
+            
+                private Conexao conexao;
+                private Connection conn;
+                
+        public FornecedorDao(){
+                    this.conexao = new Conexao();
+                    this.conn = this.conexao.conectar();
+        }
+        public void inserir(Fornecedor fornecedor){
+                        
+                    String sql = "INSERT INTO fornecedor(razaoSocial, cnpj, contato, endereco, informacoesAdicionais) VALUES (?,?,?,?,?)";
+                    
+                    try {
+                        PreparedStatement stmt = this.conn.prepareStatement(sql);
+                        stmt.setString(1, fornecedor.getRazaoSocial());
+                        stmt.setString(2, fornecedor.getCnpj());
+                        stmt.setString(3, fornecedor.getContato());
+                        stmt.setString(4, fornecedor.getEndereco());
+                        stmt.setString(5, fornecedor.getInformacoesAdicionais());
+                        stmt.execute();  
+                        
+                        
+                    } catch (Exception e) {
+                        System.out.println("Erro ao inserir fornecedor: " + e.getMessage());
+                    }
+                    
+                }
+               public List<Fornecedor> getFornecedor(String nomeFornecedor){
+                String sql = "SELECT * FROM fornecedor WHERE razaoSocial LIKE ?";
+                try {
+                    PreparedStatement stmt = this.conn.prepareStatement(sql);
+                    stmt.setString(1,"%" + nomeFornecedor+ "%");
+                    ResultSet rs = stmt.executeQuery();            
+                    
+
+                    List<Fornecedor> listaFornecedor = new ArrayList<>();
+
+                    while (rs.next()) { 
+                    Fornecedor fornecedor = new Fornecedor();
+         
+                    fornecedor.setId(rs.getInt("id"));
+                    fornecedor.setRazaoSocial(rs.getString("razaoSocial"));
+                    fornecedor.setCnpj(rs.getString("cnpj"));
+                    fornecedor.setContato(rs.getString("contato"));
+                    fornecedor.setEndereco(rs.getString("endereco"));
+                    fornecedor.setInformacoesAdicionais(rs.getString("informacoesAdicionais"));
+
+                    listaFornecedor.add(fornecedor);
+                            
+                    }
+
+                    return listaFornecedor;
+                    
+                } catch (Exception e) {
+                    return null;
+                }
+           }
+    
+}
